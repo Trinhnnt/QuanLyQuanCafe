0b898b24f12b818024d8cf43758505b988b169fb
NguyenVung0408 <nguyenthanhvung0408@gmail.com>
2025-05-23 00:44:53 +0700

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Account
+    {
+        private string userName;
+        private string displayName;
+        private int type;
+        private string password;
+        private int status;
+
+        public string UserName { get => userName; set => userName = value; }
+        public string DisplayName { get => displayName; set => displayName = value; }
+        public int Type { get => type; set => type = value; }
+        public string Password { get => password; set => password = value; }
+        public int Status { get => status; set => status = value; }
+
+        public Account(string userName, string displayName, int type, string password = null) 
+        { 
+            this.UserName = userName;
+            this.DisplayName = displayName;
+            this.Type = type;
+            this.Password = password;
+            this.Status = status;
+        }
+
+        public Account(DataRow row)
+        {
+            foreach (DataColumn col in row.Table.Columns)
+            {
+                if (string.Equals(col.ColumnName, "username", StringComparison.OrdinalIgnoreCase))
+                    this.UserName = row[col.ColumnName].ToString();
+                else if (string.Equals(col.ColumnName, "displayname", StringComparison.OrdinalIgnoreCase))
+                    this.DisplayName = row[col.ColumnName].ToString();
+                else if (string.Equals(col.ColumnName, "type", StringComparison.OrdinalIgnoreCase))
+                    this.Type = Convert.ToInt32(row[col.ColumnName]);
+                else if (string.Equals(col.ColumnName, "password", StringComparison.OrdinalIgnoreCase))
+                    this.Password = row[col.ColumnName].ToString();
+                this.Status = row["Status"] != DBNull.Value ? (int)row["Status"] : 1;
+            }
+        }
+        
+    }
+}
---
+++ AccountDAO.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using QuanLyQuanCafe.DTO;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class AccountDAO
+    {
+        private static AccountDAO instance;
+
+        public static AccountDAO Instance 
+        {
+            get { if (instance == null) instance = new AccountDAO(); return AccountDAO.instance; }
+            private set { instance = value; }
+        }
+
+        private AccountDAO() { }
+
+        public bool Login(string username, string password) 
+        {
+
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(password);
+            byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+
+            string hasPass = "";
+
+            foreach (byte item in hasData)
+            {
+                hasPass += item;
+            }
+            string query = "USP_Login @userName , @passWord";
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] {username, hasPass });
+
+            bool loginSuccess = result.Rows.Count > 0;
+
+            // Ghi log kết quả đăng nhập
+            string action = "Login";
+            string description = loginSuccess ?
+                "Đăng nhập thành công" :
+                "Đăng nhập thất bại";
+
+            ActivityLogDAO.Instance.LogActivity(
+                username,
+                action,
+                description,
+                "Account",
+                username);
+
+            return loginSuccess;
+        }
+        public Account GetAccountByUserName(string userName) 
+        {
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM account WHERE userName = '" + userName +"'");
+            foreach (DataRow item in data.Rows) 
+            {
+                return new Account(item);
+            }
+            return null;
+        }
+        public bool UpdateAccount(string userName, string displayName, string pass, string newPass)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("EXEC USP_UpdateAccount @userName , @displayName , @password , @newPassword", new object[] {userName,displayName,pass,newPass});
+            return result > 0;
+        }
+        public DataTable GetListAccount()
+        {
+            return DataProvider.Instance.ExecuteQuery("SELECT UserName, DisplayName, Type , Status FROM dbo.Account");
+        }
+        public bool InsertAccount(string name, string displayName, int type)
+        {
+            string query = string.Format("INSERT dbo.Account ( UserName, DisplayName, Type, password )VALUES  ( N'{0}', N'{1}', {2}, N'{3}')", name, displayName, type, "1962026656160185351301320480154111117132155");
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool UpdateAccount(string name, string displayName, int type)
+        {
+            string query = string.Format("UPDATE dbo.Account SET DisplayName = N'{1}', Type = {2} WHERE UserName = N'{0}'", name, displayName, type);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool DeleteAccount(string name)
+        {
+            string query = string.Format("UPDATE dbo.Account SET Status = 0 WHERE UserName = N'{0}'", name);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool ResetPassword(string name)
+        {
+            string query = string.Format("update account set password = N'1962026656160185351301320480154111117132155' where UserName = N'{0}'", name);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool RestoreAccount(string name)
+        {
+            string query = string.Format("UPDATE dbo.Account SET Status = 1 WHERE UserName = N'{0}'", name);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+    }
+}
