5ffc77d2e80b9359a54c505dd8008d0007403525
Trinh-Nguyen-Ngoc-Tu <nguyenngoctutrinh92@gmail.com>
2025-05-25 19:34:21 +0700

    log and report

---
+++ TopFoodReportDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace QuanLyQuanCafe.DTO
+{
+    public class TopFoodReportDTO
+    {
+        public string FoodName { get; set; }
+        public string CategoryName { get; set; }
+        public int TotalQuantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double TotalRevenue { get; set; }
+    }
+}
