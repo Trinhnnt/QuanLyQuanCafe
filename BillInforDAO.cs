498ea3e75dc1573c7dde007a43ce33c18b49c7b0
sodavuuu <sondang251105@gmail.com>
2025-05-27 16:53:26 +0700

    Add files via upload
    BillInfo

---
+++ BillInforDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyQuanCafe.DTO;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class BillInforDAO
+    {
+        private static BillInforDAO instance;
+
+        public static BillInforDAO Instance 
+        {
+            get { if (instance == null) instance = new BillInforDAO(); return BillInforDAO.instance; }
+            private set { BillInforDAO.instance = value; }
+        }
+
+        private BillInforDAO() { }
+
+        public List<BillInfo> GetListBillInfor(int id) 
+        {
+            List<BillInfo> listBillInfo = new List<BillInfo>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM dbo.BillInfo WHERE idBill = " + id);
+            foreach (DataRow item in data.Rows)
+            {
+                BillInfo info = new BillInfo(item);
+                listBillInfo.Add(info);
+                
+            }
+            return listBillInfo;
+        }
+        public void InsertBillInfo(int idBill, int idFood, int count)
+        {
+            DataProvider.Instance.ExecuteNonQuery("USP_InsertBillInfo @idBill , @idFood , @count ", new object[] { idBill, idFood, count });
+        }
+
+    }
+}
